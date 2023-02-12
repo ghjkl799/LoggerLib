d9a4d785f8cd80c34f376feeecc95fa2337841b6
ghjkl799 <ghjkl799@gmail.com>
2023-02-12 10:13:40 +0100

    Adds console log test

--- src/LoggerLibTests/LoggerTests.cs
+++ src/LoggerLibTests/LoggerTests.cs
@@ -1,16 +1,23 @@
+using FluentAssertions;
 using LoggerLib;
+using System.Text;
 
 namespace LoggerLibTests
 {
     public class LoggerTests
     {
         [Fact]
-        public void CanSendMessage()
+        public void CanSendMessageToConsole()
         {
             var logger = new Logger();
             var message = "mymessage";
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+            Console.SetOut(sw);
 
             logger.Log(message);
+
+            sb.ToString().Should().Be(message+Environment.NewLine);
         }
     }
 }
